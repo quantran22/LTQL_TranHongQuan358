275173f914b9770ec66c0f7fac6ab8e1ab6723d4
OhhNo <OhhNo@DESKTOP-RLHF5KE>
2021-12-21 13:18:39 +0700

    Create_Datacontext_THQ358

---
+++ LTQL_1721050358/Models/LTQLBbContex.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LTQL_1721050358.Models
+{
+    public partial class LTQLBbContex : DbContext
+    {
+        public LTQLBbContex()
+            : base("name=LTQLBbContex")
+        {
+        }
+
+        public virtual DbSet<DanhGia> DanhGias { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
