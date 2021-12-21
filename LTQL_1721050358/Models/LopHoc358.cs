6e70d77725f3a6a2a216eaba3103d092c0dabea0
OhhNo <OhhNo@DESKTOP-RLHF5KE>
2021-12-21 13:20:09 +0700

    Create_Table_LopHoc_THQ358

---
+++ LTQL_1721050358/Models/LopHoc358.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050358.Models
+{
+    public class LopHoc082
+    {
+        [Key]
+        [Display(Name = "Mã Lớp")]
+        public int Malop { get; set; }
+        [Display(Name = "Tên Lớp")]
+        [StringLength(50)]
+        public string TenLop { get; set; }
+
+    }
+}
