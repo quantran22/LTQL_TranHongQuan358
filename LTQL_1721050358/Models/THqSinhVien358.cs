4ae16d9bdf6491292a494ef5c0727d90f8250b96
OhhNo <OhhNo@DESKTOP-RLHF5KE>
2021-12-21 13:27:50 +0700

    Create_Table_SinhVien_THQ358

---
+++ LTQL_1721050358/Models/THqSinhVien358.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace LTQL_1721050358.Models
+{
+    public class THqSinhVien358
+    {
+        [Key]
+        [Display(Name = "Mã Sinh Viên")]
+        [StringLength(20)]
+        public string MaSinhVien { get; set; }
+        [Display(Name = "Họ và tên")]
+        [StringLength(50)]
+        public string HoTen { get; set; }
+        public int MaLop { get; set; }
+        [ForeignKey("MaLop")]
+    }
+}
